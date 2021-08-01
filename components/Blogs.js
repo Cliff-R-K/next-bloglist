24b09f8631832ce87a4ccba8e5461b64b0618443
burton666 <silvercircle666@gmail.com>
2021-08-01 09:22:43 +0200

    .

--- components/Blogs.js
+++ components/Blogs.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Blog from "./Blog";
 import blogService from "../services/blogs";
-import dbConnect from './../lib/dbConnect';
 
 const Blogs = ({ data }) => {
 
@@ -21,7 +20,6 @@ const [blogs, setBlogs] = useState(data);
 export default Blogs;
 
 export const getStaticProps = async () => {
-    await dbConnect()
     const data = await blogService.getAll()
 
   return {
