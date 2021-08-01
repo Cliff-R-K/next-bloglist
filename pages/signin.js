f3397ec6110ef20712a8002fcb5936f6f0e777f9
burton666 <silvercircle666@gmail.com>
2021-08-01 19:06:01 +0200

    .

---
+++ pages/signin.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useSession, signIn, signOut } from "next-auth/client";
+import { useRouter } from 'next/router'
+
+const signin = () => {
+    
+    return (
+        <div>
+           <h1>Signing</h1>
+
+        </div>
+    )
+}
+
+export default signin
