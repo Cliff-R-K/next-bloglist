1863eb964f42b94d6a4a9d1862be63eff9ed5a7e
burton666 <silvercircle666@gmail.com>
2021-08-03 13:24:19 +0200

    ...

--- pages/api/blogs/[id].js
+++ pages/api/blogs/[id].js
@@ -1,11 +1,16 @@
 import dbConnect from "./../../../lib/dbConnect";
 import Blog from "../../../models/blog";
-import getToken from "../../../utils/tokenextractor";
-import jwt from "jsonwebtoken"
+import jwt from "next-auth/jwt";
+import User from "../../../models/user";
 
 export default async (req, res) => {
   await dbConnect();
   const { id } = req.query;
+  const secret = process.env.SECRET;
+  console.log(secret)
+
+  const token = await jwt.getToken({ req, secret });
+  console.log("JSON Web Token", token);
 
   if (req.method === "GET") {
     const blogs = await Blog.find({});
@@ -15,25 +20,40 @@ export default async (req, res) => {
     }
     res.status(200).json({ Blog: blog });
   } else if (req.method === "DELETE") {
-    const token = getToken(req);
+
+    /* const token = getToken(req);
     const decodedToken = jwt.verify(token, process.env.SECRET);
     if (!token || !decodedToken.id) {
       return res.status(401).json({ error: "token missing or invalid" });
-    }
-   
-    const blogToRemove = await Blog.findById(id);
- 
-    if (blogToRemove.user.toString() !== decodedToken.id) {
+    } */
+
+    //const blogToRemove = await Blog.findById(id);
+
+    /*  if (blogToRemove.user.toString() !== decodedToken.id) {
       return res
         .status(401)
         .json({ error: "not authorized to delete specific blog" });
-    }
-    await Blog.findByIdAndRemove(id);
+    } */
+    console.log("Delete")
+    const blog = await Blog.findById(id)
+    console.log(`Blog: ${blog}`)
+    const user = await User.findOne({username:token.username})
+    console.log(`User: ${user}`)
+    //user.blogs = user.blogs.filter(b => b !== blog._id)
+    //await user.save()
+
+    console.log("user")
+    console.log(user)
+    console.log(token.username)
+    console.log(user.username)
+
+
+    //await Blog.findByIdAndRemove(id);
     res.status(200).end();
   } else if (req.method === "PUT") {
     console.log(`PUT id:${id}`);
-    const { body } = req;
-    const updatedBlog = await Blog.findByIdAndUpdate(id, body);
+    const likes = req.body.likes + 1;
+    const updatedBlog = await Blog.findByIdAndUpdate(id, { likes });
     res.status(200).json(updatedBlog.toJSON());
   }
 };
